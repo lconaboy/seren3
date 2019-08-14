e120f4b1e46fa9aba516fc4ca5f1d444f30782c6
Luke Conaboy <lukeconaboy@gmail.com>
2019-08-14 15:29:45 +0100

    Working mpi4py loop

--- analysis/drift_velocity/mpi_grafic_bias_lc.py
+++ analysis/drift_velocity/mpi_grafic_bias_lc.py
@@ -1,20 +1,21 @@
 import numpy as np
 import os
+import gc
 
 ################################################################################
 # LC - profiling memory usage
 import psutil
 def get_memory_usage():
-    """Return the memory usage in Mo."""
+    """Return the memory usage in MB."""
     process = psutil.Process(os.getpid())
-    mem = process.memory_info()[0] / float(2 ** 20)
+    mem = process.memory_info()[0] / float(1e6)
     return mem
 ################################################################################
 
 # VERBOSE = 1  # 0 for all, >0 for just patch, <0 for none
-P = False
-B = False
-C = False
+P = True
+B = True
+C = True
 
 class Result(object):
     '''
@@ -46,39 +47,6 @@ class Patch(object):
         self.field = field
 
 
-
-def work(patch, dx, rank):
-    ##########################################################
-    # LC - profile memory usage
-    print("rank {0}, memory usage = {1:.3f} Mo".format(rank, get_memory_usage()))
-    ##########################################################
-    origin = np.array(patch - float(dx) / 2. - pad, dtype=np.int64)
-    dx_eps = float(dx) + float(2 * pad)
-
-    delta = vbc = None
-    if (P): print("Loading patch: %s" % patch)
-    delta = ics.lazy_load_periodic("deltab", origin, int(dx_eps))
-    vbc = ics.lazy_load_periodic("vbc", origin, int(dx_eps))
-
-    # Compute the bias
-    if (B): print("Computing bias")
-    k_bias, b_cdm, b_b = vbc_utils.compute_bias_lc(ics, vbc)
-
-    # Convolve with field
-    if (C): print("Performing convolution")
-    delta_biased = vbc_utils.apply_density_bias(ics, k_bias, b_b, delta.shape[0], delta_x=delta)
-
-    # Remove the padded region
-    x_shape, y_shape, z_shape = delta_biased.shape
-    delta_biased = delta_biased[0 + pad:x_shape - pad,
-                                0 + pad:y_shape - pad, 0 + pad:z_shape - pad]
-
-    # Store
-    biased_patch = Patch(patch, dx, delta_biased)
-
-    return biased_patch
-
-
 def main(path, level, patch_size):
     '''
     Writes a new set of grafIC initial conditions with a drift velocity dependent
@@ -95,9 +63,9 @@ def main(path, level, patch_size):
     comm = MPI.COMM_WORLD
     size = comm.Get_size()
     rank = comm.Get_rank()
-    
+    msg = 'rank {0}: {1}'
     #mpi.msg("Loading initial conditions")
-    print("Loading initial conditions")
+    print(msg.format(rank, "Loading initial conditions"))
     
     ics = grafic_snapshot.load_snapshot(path, level, sample_fft_spacing=False)
 
@@ -113,15 +81,16 @@ def main(path, level, patch_size):
     # Compute cube positions in cell units
     cubes = dx = None
     if rank == 0:
-        print("Using %i cubes per dimension." % ncubes)
+        print(msg.format(rank, "Using {0} cubes per dimension.".format(ncubes)))
         cubes, dx = vbc_utils.cube_positions(ics, ncubes)
         cubes = np.array(cubes)
         # Split the cubes into chunks that can be scattered to each processor 
         chunks = np.array_split(cubes, size)
+    else:
+        chunks = None
     
     # dx is the same for every cube, so this is broadcast to each processor
     dx = comm.bcast(dx, root=0)
-    pad = 8
 
 
 ############################## WORK LOOP ######################################
@@ -129,41 +98,81 @@ def main(path, level, patch_size):
     # Iterate over patch positions in parallel
     # dest = {}
     patches = comm.scatter(chunks, root=0)
-    biased_patches = [work(patch, dx, rank) for patch in patches]
-    results = comm.gather(biased_patches, rank=0)
+    biased_patches = np.zeros(shape=patches.shape, dtype=object)
+    
+    for i, patch in enumerate(patches):
+        ##########################################################
+        # LC - profile memory usage
+        print("rank {0}, memory usage = {1:.3f} Mo".format(rank, get_memory_usage()))
+        ##########################################################
+
+        print(msg.format(rank, '{0}/{1}'.format(i+1, len(patches))))
+        pad = 8
+    
+        origin = np.array(patch - float(dx) / 2. - pad, dtype=np.int64)
+        dx_eps = float(dx) + float(2 * pad)
+
+        delta = vbc = None
+        if (P): print(msg.format(rank, "Loading patch: {0}").format(patch))
+        delta = ics.lazy_load_periodic("deltab", origin, int(dx_eps))
+        vbc = ics.lazy_load_periodic("vbc", origin, int(dx_eps))
+
+        # Compute the bias
+        if (B): print(msg.format(rank, "Computing bias"))
+        k_bias, b_cdm, b_b = vbc_utils.compute_bias_lc(ics, vbc)
+
+        # Convolve with field
+        if (C): print(msg.format(rank, "Performing convolution"))
+        delta_biased = vbc_utils.apply_density_bias(ics, k_bias, b_b, delta.shape[0], delta_x=delta)
+
+        # Remove the padded region
+        x_shape, y_shape, z_shape = delta_biased.shape
+        delta_biased = delta_biased[0 + pad:x_shape - pad,
+                                    0 + pad:y_shape - pad, 0 + pad:z_shape - pad]
+
+        # Store
+        biased_patches[i] = Patch(patch, dx, delta_biased)
+
+        del vbc
+        del delta
+        del delta_biased
+
+        gc.collect()
+
+
+    dest = comm.gather(biased_patches, root=0)
 
 ############################## END OF WORK LOOP ###############################
-    print ('Done!')
-    # if mpi.host:
-    #     import os
-    #     # Write new ICs
-    #     dest = mpi.unpack(dest)
+    if rank == 0:
+        import os
+        # Write new ICs
+        dest = np.ravel(dest)
 
-    #     output_field = np.zeros(ics.header.nn)
+        output_field = np.zeros(ics.header.nn)
 
-    #     for item in dest:
-    #         result = item.result
-    #         patch = result["patch"]
-    #         dx = result["dx"]
-    #         delta_biased = result["field"]
+        for item in dest:
+            result = item.result
+            patch = result["patch"]
+            dx = result["dx"]
+            delta_biased = result["field"]
 
-    #         # Bounds of this patch
-    #         x_min, x_max = (int((patch[0]) - (dx / 2.)), int((patch[0]) + (dx / 2.)))
-    #         y_min, y_max = (int((patch[1]) - (dx / 2.)), int((patch[1]) + (dx / 2.)))
-    #         z_min, z_max = (int((patch[2]) - (dx / 2.)), int((patch[2]) + (dx / 2.)))
+            # Bounds of this patch
+            x_min, x_max = (int((patch[0]) - (dx / 2.)), int((patch[0]) + (dx / 2.)))
+            y_min, y_max = (int((patch[1]) - (dx / 2.)), int((patch[1]) + (dx / 2.)))
+            z_min, z_max = (int((patch[2]) - (dx / 2.)), int((patch[2]) + (dx / 2.)))
 
-    #         # Place into output
-    #         output_field[x_min:x_max, y_min:y_max, z_min:z_max] = delta_biased
+            # Place into output
+            output_field[x_min:x_max, y_min:y_max, z_min:z_max] = delta_biased
 
-    #     # Write the initial conditions
-    #     ics_dir = "%s/ics_ramses_vbc/" % ics.level_dir
-    #     if not os.path.isdir(ics_dir):
-    #         os.mkdir(ics_dir)
-    #     out_dir = "%s/level_%03i/" % (ics_dir, level)
-    #     if not os.path.isdir(out_dir):
-    #         os.mkdir(out_dir)
+        # Write the initial conditions
+        ics_dir = "{0}/ics_ramses_vbc/".format(ics.level_dir)
+        if not os.path.isdir(ics_dir):
+            os.mkdir(ics_dir)
+        out_dir = "{0}/level_{1:03d}/".format(ics_dir, level)
+        if not os.path.isdir(out_dir):
+            os.mkdir(out_dir)
 
-    #     ics.write_field(output_field, "deltab", out_dir=out_dir)
+        ics.write_field(output_field, "deltab", out_dir=out_dir)
 
 if __name__ == "__main__":
     import sys
