8379e32259c4d3391515de9cb1b79e2339b5579d
Luke Conaboy <lukeconaboy@gmail.com>
2019-08-14 11:06:45 +0100

    Oops

--- analysis/parallel/mpi.py
+++ analysis/parallel/mpi.py
@@ -19,23 +19,23 @@ really is.
 :param ids:
 :return:
 """
-d = deep_getsizeof
-if id(o) in ids:
-    return 0
+    d = deep_getsizeof
+    if id(o) in ids:
+        return 0
 
-r = getsizeof(o)
-ids.add(id(o))
+    r = getsizeof(o)
+    ids.add(id(o))
 
-if isinstance(o, str) or isinstance(0, unicode):
+    if isinstance(o, str) or isinstance(0, unicode):
+        return r
+
+    if isinstance(o, Mapping):
+        return r + sum(d(k, ids) + d(v, ids) for k, v in o.iteritems())
+
+    if isinstance(o, Container):
+        return r + sum(d(x, ids) for x in o)
+
     return r
-
-if isinstance(o, Mapping):
-    return r + sum(d(k, ids) + d(v, ids) for k, v in o.iteritems())
-
-if isinstance(o, Container):
-    return r + sum(d(x, ids) for x in o)
-
-return r
 
 
 # Setup MPI environment
